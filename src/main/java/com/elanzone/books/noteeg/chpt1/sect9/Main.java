48d938223f70c660cfe4628c4c276bcce1fdfdde
elan <elanzone@126.com>
2015-02-17 16:23:25 +0800

    第一章第9节 处理未捕获的异常

---
+++ src/main/java/com/elanzone/books/noteeg/chpt1/sect9/Main.java
@@ -0,0 +1,15 @@
+package com.elanzone.books.noteeg.chpt1.sect9;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Task task = new Task();
+        Thread thread = new Thread(task);
+        thread.setUncaughtExceptionHandler(new ExceptionHandler());
+        thread.start();
+
+    }
+
+}
---
+++ src/main/java/com/elanzone/books/noteeg/chpt1/sect9/Task.java
@@ -0,0 +1,10 @@
+package com.elanzone.books.noteeg.chpt1.sect9;
+
+public class Task implements Runnable {
+
+    @Override
+    public void run() {
+        int numero=Integer.parseInt("TTT");
+    }
+
+}
