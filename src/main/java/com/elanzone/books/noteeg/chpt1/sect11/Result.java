fcd5b390d7fe6c3dc769c3e17a247c87af1adc29
elan <elanzone@126.com>
2015-02-17 21:47:32 +0800

    第一章第11节 线程组

---
+++ src/main/java/com/elanzone/books/noteeg/chpt1/sect11/Result.java
@@ -0,0 +1,14 @@
+package com.elanzone.books.noteeg.chpt1.sect11;
+
+public class Result {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
