99872e64581e1bc938536c02597173626bcff465
elan <elanzone@126.com>
2015-02-19 21:14:00 +0800

    第二章第7节 线程同步基础: 锁的公平 复制的第5节的代码作为本节代码的比较基准

---
+++ src/main/java/com/elanzone/books/noteeg/chpt2/sect07/PrintQueue.java
@@ -0,0 +1,25 @@
+package com.elanzone.books.noteeg.chpt2.sect07;
+
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class PrintQueue {
+
+    private final Lock queueLock = new ReentrantLock();
+
+    public void printJob(Object document) {
+        queueLock.lock();
+
+        try {
+            Long duration = (long) (Math.random() * 10000);
+            System.out.println(Thread.currentThread().getName() + ": PrintQueue: Printing a Job during " + (duration / 1000) + " seconds");
+            Thread.sleep(duration);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            queueLock.unlock();
+        }
+    }
+
+}
