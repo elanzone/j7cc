d56e9aa4ffca3f836c7701f517c0255bb415656e
elan <elanzone@126.com>
2015-02-24 02:16:49 +0800

    第四章第2节 线程executor: 创建执行者

---
+++ src/main/java/com/elanzone/books/noteeg/chpt4/sect02/Main.java
@@ -0,0 +1,14 @@
+package com.elanzone.books.noteeg.chpt4.sect02;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        for (int i = 0; i < 100; i++) {
+            Task task = new Task("Task " + i);
+            server.executeTask(task);
+        }
+        server.endServer();
+    }
+}
