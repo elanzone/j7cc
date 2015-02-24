67bf832162f3b54e7f801320415717df26b1a807
elan <elanzone@126.com>
2015-02-24 16:46:53 +0800

    第四章第7节 线程执行者: 延时执行

---
+++ src/main/java/com/elanzone/books/noteeg/chpt4/sect07/Main.java
@@ -0,0 +1,32 @@
+package com.elanzone.books.noteeg.chpt4.sect07;
+
+
+import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ScheduledThreadPoolExecutor executor = (ScheduledThreadPoolExecutor) Executors.newScheduledThreadPool(1);
+
+        System.out.printf("Main: Starting at: %s\n", new Date());
+        for (int i = 0; i < 5; i++) {
+            Task task = new Task("Task " + i);
+            executor.schedule(task, i + 1, TimeUnit.SECONDS);
+        }
+
+        executor.shutdown();
+
+        try {
+            executor.awaitTermination(1, TimeUnit.DAYS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.printf("Main: Ends at: %s\n", new Date());
+    }
+
+}
