de971603ae3576b22a7b83b05b98800f047f5848
elan <elanzone@126.com>
2015-02-24 15:32:22 +0800

    第四章第6节 线程执行者: 执行多个任务处理所有结果( Callable, invokeAll)

---
+++ src/main/java/com/elanzone/books/noteeg/chpt4/sect06/Main.java
@@ -0,0 +1,46 @@
+package com.elanzone.books.noteeg.chpt4.sect06;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ExecutorService executor = (ExecutorService) Executors.newCachedThreadPool();
+
+        List<Task> taskList = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            Task task = new Task("Task" + i);
+            taskList.add(task);
+        }
+
+        List<Future<Result>> resultList = null;
+        try {
+            resultList = executor.invokeAll(taskList);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        executor.shutdown();
+
+        System.out.println("Main: Printing the results");
+        if (resultList != null) {
+            for (Future<Result> future : resultList) {
+                try {
+                    Result result = future.get();
+                    System.out.println(result.getName() + ": " + result.
+                            getValue());
+                } catch (InterruptedException | ExecutionException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
---
+++ src/main/java/com/elanzone/books/noteeg/chpt4/sect06/Result.java
@@ -0,0 +1,24 @@
+package com.elanzone.books.noteeg.chpt4.sect06;
+
+
+public class Result {
+
+    private String name;
+    private int value;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
