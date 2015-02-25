dd08538e348fd8cc2f451ac73dfd5a045a1626b4
elan <elanzone@126.com>
2015-02-25 11:40:31 +0800

    第四章第12节 线程执行者: 管理被拒绝的任务（RejectedExecutionHandler, setRejectedExecutionHandler）

---
+++ src/main/java/com/elanzone/books/noteeg/chpt4/sect12/RejectedTaskController.java
@@ -0,0 +1,17 @@
+package com.elanzone.books.noteeg.chpt4.sect12;
+
+
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class RejectedTaskController implements RejectedExecutionHandler {
+
+    @Override
+    public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+        System.out.printf("RejectedTaskController: The task %s has been rejected\n", r.toString());
+        System.out.printf("RejectedTaskController: %s\n", executor.toString());
+        System.out.printf("RejectedTaskController: Terminating: %s\n", executor.isTerminating());
+        System.out.printf("RejectedTaskController: Terminated: %s\n", executor.isTerminated());
+    }
+
+}
---
+++ src/main/java/com/elanzone/books/noteeg/chpt4/sect12/Task.java
@@ -0,0 +1,33 @@
+package com.elanzone.books.noteeg.chpt4.sect12;
+
+
+import java.util.concurrent.TimeUnit;
+
+public class Task implements Runnable {
+
+    private String name;
+
+    public Task(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Task " + name + ": Starting");
+
+        try {
+            long duration = (long) (Math.random() * 10);
+            System.out.printf("Task %s: ReportGenerator: Generating a report during %d seconds\n", name, duration);
+            TimeUnit.SECONDS.sleep(duration);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.printf("Task %s: Ending\n", name);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
