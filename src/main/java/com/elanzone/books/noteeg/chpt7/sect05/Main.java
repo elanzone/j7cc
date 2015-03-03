82d0180c9b3311d02293da19e5fed63370de6a7f
elan <elanzone@126.com>
2015-03-03 20:03:16 +0800

    第七章第5节 定制并发类: 在Executor中使用定制线程工厂
    copy 第4节的代码，将在第4节代码的基础上修改

---
+++ src/main/java/com/elanzone/books/noteeg/chpt7/sect05/Main.java
@@ -0,0 +1,22 @@
+package com.elanzone.books.noteeg.chpt7.sect05;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MyThreadFactory factory=new MyThreadFactory("MyThreadFactory");
+        MyTask task = new MyTask();
+
+        Thread thread = factory.newThread(task);
+
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.printf("Main: Thread information.\n");
+        System.out.printf("%s\n",thread);
+        System.out.printf("Main: End of the example.\n");
+    }
+}
---
+++ src/main/java/com/elanzone/books/noteeg/chpt7/sect05/MyTask.java
@@ -0,0 +1,17 @@
+package com.elanzone.books.noteeg.chpt7.sect05;
+
+
+import java.util.concurrent.TimeUnit;
+
+public class MyTask implements Runnable {
+
+    @Override
+    public void run() {
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
