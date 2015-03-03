bc70383aa57e686ad47ca9ba3ff2477432cf2742
elan <elanzone@126.com>
2015-03-03 14:51:28 +0800

    第七章第4节 定制并发类: 线程工厂

---
+++ src/main/java/com/elanzone/books/noteeg/chpt7/sect04/MyThread.java
@@ -0,0 +1,51 @@
+package com.elanzone.books.noteeg.chpt7.sect04;
+
+
+import java.util.Date;
+
+public class MyThread extends Thread {
+    private Date creationDate;
+    private Date startDate;
+    private Date finishDate;
+
+    public MyThread(Runnable target, String name) {
+        super(target, name);
+        setCreationDate();
+    }
+
+    @Override
+    public void run() {
+        setStartDate();
+        super.run();
+        setFinishDate();
+    }
+
+    public long getExecutionTime() {
+        return finishDate.getTime() - startDate.getTime();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder buffer = new StringBuilder();
+        buffer.append(getName());
+        buffer.append(": ");
+        buffer.append(" Creation Date: ");
+        buffer.append(creationDate);
+        buffer.append(" : Running time: ");
+        buffer.append(getExecutionTime());
+        buffer.append(" Milliseconds.");
+        return buffer.toString();
+    }
+
+    private void setCreationDate() {
+        creationDate = new Date();
+    }
+
+    private void setStartDate() {
+        startDate = new Date();
+    }
+
+    private void setFinishDate() {
+        finishDate = new Date();
+    }
+}
